75f0a3fb3fcfec6288099ef352ab307ee13c0bff
Omprakash Gautam <omprakash201194@gmail.com>
2021-07-12 22:17:33 +0530

    Added alert message after successful creation of user

--- shopme-web-parent/shopme-web-backend/src/main/java/com/omprakashgautam/shopme/web/backend/controller/UserController.java
+++ shopme-web-parent/shopme-web-backend/src/main/java/com/omprakashgautam/shopme/web/backend/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -37,9 +38,10 @@ public class UserController {
     }
 
     @PostMapping("/users/save")
-    public String saveUser(User user){
+    public String saveUser(User user, RedirectAttributes redirectAttributes){
         System.out.println(user);
         userService.save(user);
+        redirectAttributes.addFlashAttribute("message","The user has been saved successfully.");
         return "redirect:/users";
     }
 }
