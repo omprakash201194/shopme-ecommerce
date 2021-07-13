ba2d3a1ab7db68ce7c917ee053a8a4eca2750769
Omprakash Gautam <omprakash201194@gmail.com>
2021-07-13 13:54:45 +0530

    Configured Password Encoder to encode password while user registration

---
+++ shopme-web-parent/shopme-web-backend/src/main/java/com/omprakashgautam/shopme/web/backend/security/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.omprakashgautam.shopme.web.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author omprakash gautam
+ * Created on 13-Jul-21 at 1:46 PM.
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll();
+    }
+}
--- shopme-web-parent/shopme-web-backend/src/main/java/com/omprakashgautam/shopme/web/backend/service/UserService.java
+++ shopme-web-parent/shopme-web-backend/src/main/java/com/omprakashgautam/shopme/web/backend/service/UserService.java
@@ -5,6 +5,7 @@ import com.omprakashgautam.shopme.commons.entity.User;
 import com.omprakashgautam.shopme.web.backend.repository.RoleRepository;
 import com.omprakashgautam.shopme.web.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,6 +22,9 @@ public class UserService {
     @Autowired
     private RoleRepository roleRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public List<User> listAll(){
         return userRepository.findAll();
     }
@@ -30,6 +34,12 @@ public class UserService {
     }
 
     public void save(User user) {
+        encodePassword(user);
         userRepository.save(user);
+    }
+
+    private void encodePassword(User user){
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
     }
 }
