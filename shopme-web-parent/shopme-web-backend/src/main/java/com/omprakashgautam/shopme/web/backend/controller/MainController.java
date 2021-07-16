0c9c905f80c1a21d4de234f99f39c723912ee801
Omprakash Gautam <omprakash201194@gmail.com>
2021-07-16 18:10:03 +0530

    Added user-login page

--- shopme-web-parent/shopme-web-backend/src/main/java/com/omprakashgautam/shopme/web/backend/controller/MainController.java
+++ shopme-web-parent/shopme-web-backend/src/main/java/com/omprakashgautam/shopme/web/backend/controller/MainController.java
@@ -14,4 +14,9 @@ public class MainController {
     public String viewHomePage(){
         return "index";
     }
+
+    @GetMapping("/login")
+    public String viewLoginPage(){
+        return "login";
+    }
 }
--- shopme-web-parent/shopme-web-backend/src/main/java/com/omprakashgautam/shopme/web/backend/security/WebSecurityConfig.java
+++ shopme-web-parent/shopme-web-backend/src/main/java/com/omprakashgautam/shopme/web/backend/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.omprakashgautam.shopme.web.backend.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,6 +24,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("email")
+                .permitAll();
+    }
+
+    //Access to static resources
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/images/**","/js/**","/webjars/**");
     }
 }
